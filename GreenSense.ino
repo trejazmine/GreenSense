b757d0e54b84bb198e728eae66a38321e45efc83
Jazmin Trejo Pérez <129553255+trejazmine@users.noreply.github.com>
2024-01-09 23:08:42 -0500

    Update GreenSense.ino

--- GreenSense.ino
+++ GreenSense.ino
@@ -1,10 +1,10 @@
 #include "LedControl.h"
 
-LedControl lc = LedControl(11, 13, 10, 1);
+LedControl lc = LedControl(11,13,10,1); //DIN,CLK,CS,#DISP
 
 #define demora 500
-
-const int sensorPin = A0;
+#define sensorPin A0 
+int valorSensor = 0;
 
 byte feliz[8] = {
 B00000000, // filas necesarias por mostrar el digito cero			
@@ -12,7 +12,7 @@ B00100100,
 B00100100,			
 B00000000,			
 B01000010,			
-B00100100,			
+B00100100,		
 B00011000,			
 B00000000		
 };
@@ -41,8 +41,8 @@ byte triste[8] = {
 
 byte morido[8] = {
   B00000000, // filas necesarias por mostrar el digito cero			
-  B01100110,		
-  B01100110,				
+  B00100100,		
+  B00100100,				
   B00000000,	
   B00000000,					
   B00101010,			
@@ -50,13 +50,24 @@ byte morido[8] = {
   B00000000
 };
 
-byte nada[8] = {
+byte mudo[8] = {
   B00000000, // filas necesarias por mostrar el digito cero			
   B00100100,		
   B00100100,				
   B00000000,	
   B00000000,					
   B00000000,			
+  B00000000,	
+  B00000000
+};
+
+byte nada[8] = {
+  B00000000,
+  B01000010, // filas necesarias por mostrar el digito cero			
+  B00100100,						
+  B00010000,						
+  B00101000,			
+  B01000100,
   B00000000,	
   B00000000
 };
@@ -69,20 +80,32 @@ void setup() {
 }
 
 void loop() {
-  int valorSensor = analogRead(sensorPin);
-  Serial.print("Valor del sensor: ");
-  Serial.println(valorSensor);
 
-  if (valorSensor >= 600) {
-    mostrar_morido();
-  } else if (valorSensor >= 500 && valorSensor < 600) {
-    mostrar_serio();
-  } else if (valorSensor >= 100 && valorSensor < 500) {
+  int valorSensor = map(analogRead(sensorPin), 0, 1023, 100, 0);
+  //se hace un mapeo de la lectura del sensor a porcentual
+  Serial.print("Humedad de la tierra: ");
+  Serial.print(valorSensor);
+  Serial.println("%");
+  delay(100);
+
+
+  if (valorSensor >= 80 && valorSensor < 100) {
+    mostrar_morido();//mareado
+  } 
+  else if (valorSensor >=65 && valorSensor < 80) {
+    mostrar_mudo();
+  }
+  else if (valorSensor >= 55 && valorSensor < 65) {
     mostrar_feliz();
-  } else if (valorSensor >= 0 && valorSensor < 100) {
+  }
+  else if (valorSensor >= 45 && valorSensor <= 55) {
     mostrar_triste();
-  //} else if (valorSensor >= 0 && valorSensor < 20) {
-  //  mostrar_nada();
+  }
+  else if (valorSensor >= 5 && valorSensor < 45) {
+    mostrar_serio();
+  }
+  else if (valorSensor >= 0 && valorSensor < 5) {
+    mostrar_nada();
   }
 
   delay(demora);
@@ -113,6 +136,13 @@ void mostrar_morido() {
  for (int i = 0; i < 8; i++) 		// bucle itera 8 veces por el array 
   {
     lc.setRow(0,i,morido[i]);		// establece fila con valor de array tres en misma posicion
+  }
+}
+
+void mostrar_mudo() {
+ for (int i = 0; i < 8; i++) 		// bucle itera 8 veces por el array 
+  {
+    lc.setRow(0,i,mudo[i]);		// establece fila con valor de array tres en misma posicion
   }
 }
 
